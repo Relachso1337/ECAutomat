68531a2bbc62e9b29e0c4b26f9e06511cdf970a0
Cfriedr1ch <christianfriedrich06@gmail.com>
2022-12-16 00:07:01 +0100

    json parsing with testobject

--- ECAutomat-Frontend/src/main/java/de/hsog/routing/DefaultRouter.java
+++ ECAutomat-Frontend/src/main/java/de/hsog/routing/DefaultRouter.java
@@ -1,8 +1,20 @@
 package de.hsog.routing;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.ResourceUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.fasterxml.jackson.core.exc.StreamReadException;
+import com.fasterxml.jackson.databind.DatabindException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import de.hsog.mensaplan.MensaMenu;
 
 
 @Controller
@@ -12,5 +24,28 @@ public class DefaultRouter{
 	@GetMapping(value = "")
 	public String index() {
 		return "html/index";
+	}
+	
+	@GetMapping(value = "mensaplan")
+	public String mensaplan(Model model) {	
+		try {
+			File file = ResourceUtils.getFile("classpath:static/media/misc/menu.json");
+			ObjectMapper objectMapper = new ObjectMapper();
+			MensaMenu menu = objectMapper.readValue(file, MensaMenu.class);
+			model.addAttribute("speisen", menu.getMenu());
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (StreamReadException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (DatabindException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return "html/mensaplan";
 	}
 }
