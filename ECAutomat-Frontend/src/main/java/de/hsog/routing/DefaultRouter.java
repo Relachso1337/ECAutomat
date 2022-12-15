09d749f548f3e33b37f813f6248650553d011634
Cfriedr1ch <christianfriedrich06@gmail.com>
2022-12-15 21:23:42 +0100

    minor changes

--- ECAutomat-Frontend/src/main/java/de/hsog/routing/DefaultRouter.java
+++ ECAutomat-Frontend/src/main/java/de/hsog/routing/DefaultRouter.java
@@ -4,12 +4,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+
 @Controller
 @RequestMapping("/")
 public class DefaultRouter{
 	
 	@GetMapping(value = "")
 	public String index() {
-		return "html/vorlesungsplan";
+		return "html/index";
 	}
 }
