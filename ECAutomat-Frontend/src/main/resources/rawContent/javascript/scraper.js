38e28b4615fa2856d80d6a53dcb4ff9734998d69
Cfriedr1ch <christianfriedrich06@gmail.com>
2022-12-16 00:05:36 +0100

    mensaplan inital commit

---
+++ ECAutomat-Frontend/src/main/resources/rawContent/javascript/scraper.js
@@ -0,0 +1,34 @@
+/*Mensaplan Truong*/
+const puppeteer = require('puppeteer');
+const fs = require('fs/promises');
+
+async function scrapeProduct(url) {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(url);
+
+    const [el] = await page.$x('//*[@id="tab-thu"]/div/div[1]/small');
+    const menu = await el.getProperty('textContent');
+    const menuTxt = await menu.jsonValue();
+
+    const [el2] = await page.$x('//*[@id="tab-thu"]/div/div[2]/small');
+    const menu2 = await el2.getProperty('textContent');
+    const menuTxt2 = await menu2.jsonValue();
+
+    console.log(menuTxt, menuTxt2);
+
+    var obj = {
+        menu: []
+     };
+
+    obj.menu.push({id: 1, essen: menuTxt});
+    obj.menu.push({id: 2, essen: menuTxt2});
+
+    var json = JSON.stringify(obj);
+
+    fs.writeFile('menu.json', json, 'utf8');
+
+    browser.close();
+}
+
+scrapeProduct('https://www.swfr.de/essen/mensen-cafes-speiseplaene/mensa-offenburg');
