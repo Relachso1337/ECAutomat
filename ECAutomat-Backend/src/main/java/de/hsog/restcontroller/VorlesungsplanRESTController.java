0ac305ef335dd591fc59fcae550e8ec1ae0415f4
Cfriedr1ch <christianfriedrich06@gmail.com>
2022-12-17 18:47:09 +0100

    added Cross-Origin (CORS) Allowance to Controller

--- ECAutomat-Backend/src/main/java/de/hsog/restcontroller/VorlesungsplanRESTController.java
+++ ECAutomat-Backend/src/main/java/de/hsog/restcontroller/VorlesungsplanRESTController.java
@@ -2,44 +2,49 @@ package de.hsog.restcontroller;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.IOException;
 
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.ResourceUtils;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.fasterxml.jackson.core.exc.StreamReadException;
-import com.fasterxml.jackson.databind.DatabindException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 
 @RestController
 @RequestMapping(value = "/REST")
 public class VorlesungsplanRESTController {
+	
+	private final ObjectMapper mapper;
+	
+	public VorlesungsplanRESTController() {
+		this.mapper = new ObjectMapper();
+	}
 
+	
+	@CrossOrigin(allowedHeaders = "Access-Control-Allow-Origin", origins = "*")
 	@GetMapping(value = "/vorlesung", produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<String> vorlesungsJSON() {
+		// TODO: Added Wildcard to Access-Control-Allow-Origin CORS-Header, could be dangerous
+		String responseBody;
 		try {
 			File file = ResourceUtils.getFile("classpath:static/vorlesungsplan.json");
-			ObjectMapper mapper = new ObjectMapper();
 			Object json = mapper.readValue(file, Object.class);
-			HttpHeaders responseHeaders = new HttpHeaders();
-			responseHeaders.set("Access-Control-Allow-Origin", "*");
-			return new ResponseEntity<String>(mapper.writerWithDefaultPrettyPrinter().writeValueAsString(json), HttpStatus.OK);
+			return new ResponseEntity<String>(this.mapper.writerWithDefaultPrettyPrinter().writeValueAsString(json), HttpStatus.OK);
 		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (StreamReadException e) {
-			e.printStackTrace();
-		} catch (DatabindException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
+			responseBody = "Could not find File on Server";
+		} catch (Exception e) {
+			responseBody = e.toString();
+		}
+		try {
+			responseBody = this.mapper.writeValueAsString(responseBody);
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		return new ResponseEntity<String>("{\"error\": \"File not found\"}", HttpStatus.NOT_FOUND);
+		return new ResponseEntity<String>(responseBody, HttpStatus.NOT_FOUND);
 	}
 }
