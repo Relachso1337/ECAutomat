80885709ed43b07bfc61e4b064751362650e751f
Simon <105153894+Relachso1337@users.noreply.github.com>
2022-12-19 21:25:31 +0100

    Vorlesungsplan Seite fertig

--- ECAutomat-Frontend/src/main/resources/rawContent/javascript/vorlesungsplan.js
+++ ECAutomat-Frontend/src/main/resources/rawContent/javascript/vorlesungsplan.js
@@ -4,10 +4,12 @@ const searchInput = document.querySelector("[data-search]")
 
 let users = []
 
+
+
 searchInput.addEventListener("input", e => {
     const value = e.target.value.toLowerCase()
     users.forEach(user => {
-        const isVisible = user.semester.toLowerCase().includes(value) || user.vorlesungsplan.toLowerCase().includes(value)
+        const isVisible = user.semester.toLowerCase().includes(value)
         user.element.classList.toggle("hide", !isVisible)
     })
    
@@ -17,13 +19,19 @@ fetch("http://87.162.240.81:8888/api/v1/REST/vorlesung")
 .then(data => {
     users = data.map(user => {
         const card = userCardTemplate.content.cloneNode(true).children[0]
-        const link = card.querySelector("[data-link]")
+        //const link = card.querySelector("[data-link]")
         const header = card.querySelector("[data-header]")
         const body = card.querySelector("[data-body]")
-        link.href = user.vorlesungsplan
+        card.link = user.vorlesungsplan
+        //link.src = user.vorlesungsplan
         header.textContent = user.semester
         body.textContent = user.studiengang
         userCardContainer.append(card)
         return { vorlesungsplan: user.vorlesungsplan, semester: user.semester, studiengang: user.studiengang, element : card}
     })
 })
+
+function anklicken(e){
+   const iframe = document.getElementById("fenster")
+   iframe.src = e.link
+}
