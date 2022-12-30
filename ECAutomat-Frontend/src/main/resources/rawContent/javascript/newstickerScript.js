86c6ac8d52347d942716ae198b7600ea8b6070d3
Cfriedr1ch <christianfriedrich06@gmail.com>
2022-12-30 21:56:44 +0100

    added animation to mensa

--- ECAutomat-Frontend/src/main/resources/rawContent/javascript/newstickerScript.js
+++ ECAutomat-Frontend/src/main/resources/rawContent/javascript/newstickerScript.js
@@ -1,10 +1,10 @@
-// setTimeout(function() {
-//     var container = document.getElementById("redcontainer");
-//     container.classList.remove("slideOut");
-//     void container.offsetWidth;
-//     container.classList.add("slideOut");
-//     setTimeout(function() {
-//         // window.location.replace("../html/mensa.html");
-//     }, 1400);
+setTimeout(function() {
+    var container = document.getElementById("redcontainer");
+    container.classList.remove("slideOut");
+    void container.offsetWidth;
+    container.classList.add("slideOut");
+    setTimeout(function() {
+        window.location.replace("../html/mensa.html");
+    }, 1400);
     
-// }, 18000);
+}, 18000);
